1a3739016914b40972e3f2461c6e9265dafbd3ef
tpltnt <tpltnt.github@dropcut.net>
2012-09-12 14:36:32 +0200

    proper license disclaimer added

--- freebsd-arm-toolchain.sh
+++ freebsd-arm-toolchain.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 #
-# a small script to (natively) build an ARM toolchain for Cortex M3
-# on FreeBSD 9.0
+# a small script to (natively) build an ARM toolchain for Cortex M3 on FreeBSD
 #
+#    Copyright (C) 2012  tpltnt
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 mkdir -p ~/arm-toolchain/native-gcc/src
 wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.3.6/gcc-4.3.6.tar.bz2
